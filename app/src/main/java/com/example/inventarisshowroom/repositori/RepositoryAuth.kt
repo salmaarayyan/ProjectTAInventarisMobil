0c6b4cb15454c8054349fbd107237a2f3aed37be
salmaarayyan <salmaarayyan@gmail.com>
2025-12-25 12:38:35 +0700

    Menambahkan implementasi `JaringanRepositoryAuth` untuk menangani proses login dan logout melalui `ServiceApiShowroom`

--- app/src/main/java/com/example/inventarisshowroom/repositori/RepositoryAuth.kt
+++ app/src/main/java/com/example/inventarisshowroom/repositori/RepositoryAuth.kt
@@ -2,9 +2,25 @@ package com.example.inventarisshowroom.repositori
 
 import com.example.inventarisshowroom.modeldata.LoginResponse
 import retrofit2.Response
+import com.example.inventarisshowroom.apiservice.ServiceApiShowroom
+import com.example.inventarisshowroom.modeldata.LoginRequest
 
 interface RepositoryAuth {
     suspend fun login(email: String, password: String): LoginResponse
     suspend fun logout(token: String): Response<Void>
+}
+
+class JaringanRepositoryAuth(
+    private val serviceApiShowroom: ServiceApiShowroom
+) : RepositoryAuth {
+
+    override suspend fun login(email: String, password: String): LoginResponse {
+        val loginRequest = LoginRequest(email, password)
+        return serviceApiShowroom.login(loginRequest)
+    }
+
+    override suspend fun logout(token: String): Response<Void> {
+        return serviceApiShowroom.logout("Bearer $token")
+    }
 }
 
