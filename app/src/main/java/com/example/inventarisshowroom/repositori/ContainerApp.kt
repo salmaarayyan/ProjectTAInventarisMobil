a65e4be99fd0d85725d2b3c2eac8017194fe1e83
salmaarayyan <salmaarayyan@gmail.com>
2025-12-25 12:35:14 +0700

    Menambahkan interface `AppContainer` untuk manajemen dependency injection repository (Merk, Mobil, dan Auth)

---
+++ app/src/main/java/com/example/inventarisshowroom/repositori/ContainerApp.kt
@@ -0,0 +1,7 @@
+package com.example.inventarisshowroom.repositori
+
+interface AppContainer {
+    val repositoryMerk: RepositoryMerk
+    val repositoryMobil: RepositoryMobil
+    val repositoryAuth: RepositoryAuth
+}
