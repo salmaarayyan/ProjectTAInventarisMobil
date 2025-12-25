96277313f86800c601d97122689da7f2a728b0a7
salmaarayyan <salmaarayyan@gmail.com>
2025-12-25 12:50:18 +0700

    Menambahkan object `DestinasiDashboard` untuk pengaturan navigasi ke halaman dashboard

---
+++ app/src/main/java/com/example/inventarisshowroom/uicontroller/route/DestinasiDashboard.kt
@@ -0,0 +1,6 @@
+package com.example.inventarisshowroom.uicontroller.route
+
+object DestinasiDashboard : DestinasiNavigasi {
+    override val route = "dashboard"
+    override val titleRes = "Dashboard"
+}
