5b549075a34ae3d02fed4423c4f0e9a19b745078
salmaarayyan <salmaarayyan@gmail.com>
2025-12-26 14:04:09 +0700

    Menambahkan fungsi `loadMobilList` pada `ListMobilViewModel` untuk memuat data daftar mobil berdasarkan ID merk

--- app/src/main/java/com/example/inventarisshowroom/viewmodel/ListMobilViewModel.kt
+++ app/src/main/java/com/example/inventarisshowroom/viewmodel/ListMobilViewModel.kt
@@ -7,6 +7,9 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.inventarisshowroom.repositori.RepositoryMobil
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 
 sealed class ListMobilUiState {
@@ -31,6 +34,20 @@ class ListMobilViewModel(
     fun setCurrentMerk(merkId: Int, merkName: String) {
         currentMerkId = merkId
         currentMerkName = merkName
+    }
+
+    fun loadMobilList(token: String, merkId: Int) {
+        viewModelScope.launch {
+            listMobilUiState = ListMobilUiState.Loading
+            listMobilUiState = try {
+                val mobilList = repositoryMobil.getMobilList(token, merkId)
+                ListMobilUiState.Success(mobilList)
+            } catch (e: IOException) {
+                ListMobilUiState.Error
+            } catch (e: HttpException) {
+                ListMobilUiState.Error
+            }
+        }
     }
 
 }
