9c4ba2b3ba2ddfaa57f9614d8a421cf13a1b78c4
salmaarayyan <salmaarayyan@gmail.com>
2025-12-26 14:10:54 +0700

    Menambahkan `FormMobilViewModel` dan `FormMobilState` untuk pengelolaan state pada form mobil

--- app/src/main/java/com/example/inventarisshowroom/viewmodel/FormMobilViewModel.kt
+++ app/src/main/java/com/example/inventarisshowroom/viewmodel/FormMobilViewModel.kt
@@ -1,5 +1,10 @@
 package com.example.inventarisshowroom.viewmodel
 
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.inventarisshowroom.repositori.RepositoryMobil
 
 data class FormMobilState(
     val namaMobil: String = "",
@@ -19,3 +24,18 @@ data class FormMobilState(
     val stokError: String? = null
 )
 
+class FormMobilViewModel(
+    private val repositoryMobil: RepositoryMobil
+) : ViewModel() {
+
+    var formState by mutableStateOf(FormMobilState())
+        private set
+
+    var isEditMode by mutableStateOf(false)
+        private set
+
+    var editMobilId by mutableStateOf(0)
+        private set
+
+
+}
