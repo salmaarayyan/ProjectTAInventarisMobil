f1f5f4e2e7bbf7e9619b3c3557b40ee45013b9c5
salmaarayyan <salmaarayyan@gmail.com>
2025-12-26 15:21:54 +0700

    Mengatur `MainActivity` dengan integrasi `NavHostController` dan komponen `PetaNavigasi` menggunakan Jetpack Compose

---
+++ app/src/main/java/com/example/inventarisshowroom/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.example.inventarisshowroom
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
+import com.example.inventarisshowroom.ui.theme.InventarisShowroomTheme
+import com.example.inventarisshowroom.uicontroller.PetaNavigasi
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            InventarisShowroomTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    val navController = rememberNavController()
+                    PetaNavigasi(navController = navController)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    InventarisShowroomTheme {
+        Greeting("Android")
+    }
+}
