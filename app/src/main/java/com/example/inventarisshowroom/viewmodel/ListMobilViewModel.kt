2a14fd4a982d22f148b4ad66997f09a87702de2d
salmaarayyan <salmaarayyan@gmail.com>
2025-12-26 14:02:29 +0700

    Menambahkan `ListMobilViewModel` serta state `ListMobilUiState` untuk pengelolaan data daftar mobil

--- app/src/main/java/com/example/inventarisshowroom/viewmodel/ListMobilViewModel.kt
+++ app/src/main/java/com/example/inventarisshowroom/viewmodel/ListMobilViewModel.kt
@@ -1,6 +1,13 @@
 package com.example.inventarisshowroom.viewmodel
 
 import com.example.inventarisshowroom.modeldata.DataMobil
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.inventarisshowroom.repositori.RepositoryMobil
+
 
 sealed class ListMobilUiState {
     data class Success(val mobilList: List<DataMobil>) : ListMobilUiState()
@@ -8,4 +15,19 @@ sealed class ListMobilUiState {
     object Loading : ListMobilUiState()
 }
 
+class ListMobilViewModel(
+    private val repositoryMobil: RepositoryMobil
+) : ViewModel() {
+
+    var listMobilUiState: ListMobilUiState by mutableStateOf(ListMobilUiState.Loading)
+        private set
+
+    var currentMerkId by mutableStateOf(0)
+        private set
+
+    var currentMerkName by mutableStateOf("")
+        private set
+
+
+}
 
