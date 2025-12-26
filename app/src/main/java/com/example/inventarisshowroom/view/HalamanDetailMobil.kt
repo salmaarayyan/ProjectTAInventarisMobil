04b926cc6258d818cb54465579298ac8fd05c16b
salmaarayyan <salmaarayyan@gmail.com>
2025-12-26 15:07:46 +0700

    Menambahkan logika pengambilan token dari `UserPreferences` dan inisialisasi pemanggilan `loadDetailMobil` pada `HalamanDetailMobil`

--- app/src/main/java/com/example/inventarisshowroom/view/HalamanDetailMobil.kt
+++ app/src/main/java/com/example/inventarisshowroom/view/HalamanDetailMobil.kt
@@ -5,6 +5,8 @@ import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.inventarisshowroom.viewmodel.DetailMobilViewModel
 import com.example.inventarisshowroom.viewmodel.provider.PenyediaViewModel
+import androidx.compose.ui.platform.LocalContext
+import com.example.inventarisshowroom.local.UserPreferences
 
 @Composable
 fun HalamanDetailMobil(
@@ -14,6 +16,12 @@ fun HalamanDetailMobil(
     modifier: Modifier = Modifier,
     viewModel: DetailMobilViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ) {
+    val context = LocalContext.current
+    val userPreferences = remember { UserPreferences(context) }
+    val token = userPreferences.getToken() ?: ""
 
+    LaunchedEffect(mobilId) {
+        viewModel.loadDetailMobil(token, mobilId)
+    }
 }
 
