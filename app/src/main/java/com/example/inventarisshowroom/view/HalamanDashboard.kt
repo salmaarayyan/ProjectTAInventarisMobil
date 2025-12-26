9de5345de79be14fede09575afd4ba9b6a59f673
salmaarayyan <salmaarayyan@gmail.com>
2025-12-26 14:26:06 +0700

    Mengimplementasikan pengambilan data merk menggunakan `DashboardViewModel` dan inisialisasi `UserPreferences` pada `HalamanDashboard`

--- app/src/main/java/com/example/inventarisshowroom/view/HalamanDashboard.kt
+++ app/src/main/java/com/example/inventarisshowroom/view/HalamanDashboard.kt
@@ -1,11 +1,12 @@
 package com.example.inventarisshowroom.view
 
-
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.inventarisshowroom.viewmodel.DashboardViewModel
 import com.example.inventarisshowroom.viewmodel.provider.PenyediaViewModel
+import androidx.compose.ui.platform.LocalContext
+import com.example.inventarisshowroom.local.UserPreferences
 
 @Composable
 fun HalamanDashboard(
@@ -13,4 +14,14 @@ fun HalamanDashboard(
     onLogout: () -> Unit,
     modifier: Modifier = Modifier,
     viewModel: DashboardViewModel = viewModel(factory = PenyediaViewModel.Factory)
-){}
+){
+    val context = LocalContext.current
+    val userPreferences = remember { UserPreferences(context) }
+    val token = userPreferences.getToken() ?: ""
+    val scope = rememberCoroutineScope()
+    var showLogoutDialog by remember { mutableStateOf(false) }
+
+    LaunchedEffect(Unit) {
+        viewModel.loadMerkList(token)
+    }
+}
