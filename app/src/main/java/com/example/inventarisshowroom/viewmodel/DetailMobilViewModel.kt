14a00439f746fc45cfb0fb626c054e4e01da544d
salmaarayyan <salmaarayyan@gmail.com>
2025-12-26 14:07:54 +0700

    Menambahkan fungsi `loadDetailMobil` pada `DetailMobilViewModel` untuk memuat data detail mobil berdasarkan ID

--- app/src/main/java/com/example/inventarisshowroom/viewmodel/DetailMobilViewModel.kt
+++ app/src/main/java/com/example/inventarisshowroom/viewmodel/DetailMobilViewModel.kt
@@ -7,6 +7,9 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.inventarisshowroom.repositori.RepositoryMobil
+import kotlinx.coroutines.launch
+import retrofit2.HttpException
+import java.io.IOException
 
 sealed class DetailMobilUiState {
     data class Success(val mobilDetail: MobilDetail) : DetailMobilUiState()
@@ -21,6 +24,19 @@ class DetailMobilViewModel(
     var detailMobilUiState: DetailMobilUiState by mutableStateOf(DetailMobilUiState.Loading)
         private set
 
-
+    // Load detail mobil
+    fun loadDetailMobil(token: String, id: Int) {
+        viewModelScope.launch {
+            detailMobilUiState = DetailMobilUiState.Loading
+            detailMobilUiState = try {
+                val response = repositoryMobil.getMobilDetail(token, id)
+                DetailMobilUiState.Success(response)
+            } catch (e: IOException) {
+                DetailMobilUiState.Error
+            } catch (e: HttpException) {
+                DetailMobilUiState.Error
+            }
+        }
+    }
 }
 
