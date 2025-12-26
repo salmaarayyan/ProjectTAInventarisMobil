c4ea020a44f29adb7aedd2dc137f3ec18ef9b5f1
salmaarayyan <salmaarayyan@gmail.com>
2025-12-26 14:07:24 +0700

    Menambahkan class `DetailMobilViewModel` dan `DetailMobilUiState` untuk pengelolaan state detail mobil menggunakan Compose State dan ViewModel

--- app/src/main/java/com/example/inventarisshowroom/viewmodel/DetailMobilViewModel.kt
+++ app/src/main/java/com/example/inventarisshowroom/viewmodel/DetailMobilViewModel.kt
@@ -1,10 +1,26 @@
 package com.example.inventarisshowroom.viewmodel
 
 import com.example.inventarisshowroom.modeldata.MobilDetail
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.inventarisshowroom.repositori.RepositoryMobil
 
 sealed class DetailMobilUiState {
     data class Success(val mobilDetail: MobilDetail) : DetailMobilUiState()
     object Error : DetailMobilUiState()
     object Loading : DetailMobilUiState()
+}
+
+class DetailMobilViewModel(
+    private val repositoryMobil: RepositoryMobil
+) : ViewModel() {
+
+    var detailMobilUiState: DetailMobilUiState by mutableStateOf(DetailMobilUiState.Loading)
+        private set
+
+
 }
 
